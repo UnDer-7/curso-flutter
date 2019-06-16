4da63e68bbb7e9c8a195aa8d79ac70fe3ae7ff1d
UnDer-7 <mateus7532@gmail.com>
2019-06-16 18:43:57 -0300

    Class 74 - Adding the Sidedrawer & the Hamburger Icon

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -4,6 +4,20 @@ import '../product_manager.dart';
 class ProductsPage extends StatelessWidget {
     @override
     Widget build(BuildContext context) => Scaffold(
+        drawer: Drawer(
+            child: Column(
+                children: <Widget>[
+                    AppBar(
+                        automaticallyImplyLeading: false,
+                        title: Text('Choose'),
+                    ),
+                    ListTile(
+                        title: Text('Manage Products'),
+                        onTap: () {},
+                    ),
+                ],
+            ),
+        ),
         appBar: AppBar(
             title: Text('EasyList'),
         ),
