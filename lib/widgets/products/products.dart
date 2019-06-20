7c4d1683916ecc071bf1de848ab558a35fcd1f8e
UnDer-7 <mateus7532@gmail.com>
2019-06-20 20:37:59 -0300

    class 112 - Refactoring our Code

--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './price_tag.dart';
+import './product_card.dart';
 
 class Products extends StatelessWidget {
     final List<Map<String, dynamic>> products;
@@ -10,69 +10,13 @@ class Products extends StatelessWidget {
     @override
     Widget build(BuildContext context) => _buildProductList();
 
-    Widget _buildProductItem(BuildContext context, int index) {
-        return Card(
-            child: Column(
-                children: <Widget>[
-                    Image.asset(products[index]['image']),
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                                Text(
-                                    products[index]['title'],
-                                    style: TextStyle(
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Oswald',
-                                    ),
-                                ),
-                                SizedBox(width: 8),
-                                PriceTag(products[index]['price'].toString()),
-                            ],
-                        ),
-                    ),
-                    Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 6,
-                            vertical: 2.5,
-                        ),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(
-                                color: Colors.grey,
-                                width: 1,
-                            ),
-                        ),
-                        child: Text('Sobradinho II'),
-                    ),
-                    ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                            IconButton(
-                                icon: Icon(Icons.info),
-                                color: Theme.of(context).accentColor,
-                                onPressed: () => _navigateTo(index, context: context),
-                            ),
-                            IconButton(
-                                icon: Icon(Icons.favorite_border),
-                                color: Colors.red,
-                                onPressed: () {},
-                            ),
-                        ],
-                    )
-                ],
-            ),
-        );
-    }
 
     Widget _buildProductList() {
         Widget productCard = Center(child: Text('No Products found!'));
 
         if (products.length > 0) {
             productCard = ListView.builder(
-                itemBuilder: _buildProductItem,
+                itemBuilder: (BuildContext context, int index) => ProductCard(products[index], index),
                 itemCount: products.length,
             );
         }
@@ -80,10 +24,5 @@ class Products extends StatelessWidget {
         return productCard;
     }
 
-    void _navigateTo(index, {final BuildContext context}) {
-        Navigator.pushNamed<bool>(
-            context,
-            '/product/' + index.toString(),
-        );
-    }
+
 }
