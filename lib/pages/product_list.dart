7d0decce95c8152810c6eaa7f6cbc65aace04593
UnDer-7 <mateus7532@gmail.com>
2019-06-23 16:39:52 -0300

    class 138 - Improving the List Tile

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -12,21 +12,29 @@ class ProductListPage extends StatelessWidget {
         ListView.builder(
             itemCount: products.length,
             itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                    leading: Image.asset(products[index]['image']),
-                    title: Text(products[index]['title']),
-                    trailing: IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) => ProductEditPage(
-                                    product: products[index],
-                                    updateProduct: updateProduct,
-                                    productIndex: index,
-                                ),
-                            ));
-                        },
-                    ),
+                return Column(
+                    children: <Widget>[
+                        ListTile(
+                            leading: CircleAvatar(
+                                backgroundImage: AssetImage(products[index]['image']),
+                            ),
+                            title: Text(products[index]['title']),
+                            subtitle: Text('\$${products[index]['price'].toString()}'),
+                            trailing: IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (BuildContext context) => ProductEditPage(
+                                            product: products[index],
+                                            updateProduct: updateProduct,
+                                            productIndex: index,
+                                        ),
+                                    ));
+                                },
+                            ),
+                        ),
+                        Divider(),
+                    ],
                 );
             },
         );
